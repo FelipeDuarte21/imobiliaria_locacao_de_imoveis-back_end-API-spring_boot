a68e3d46d45982cfec0516e7312a53b782233c60
Felipe Duarte <felipe15lfde@gmail.com>
2020-09-20 03:41:41 -0300

    Acrescimo de um atributo na classe AluguelDTO

--- src/main/java/br/com/felipeDuarte/domain/dto/AluguelDTO.java
+++ src/main/java/br/com/felipeDuarte/domain/dto/AluguelDTO.java
@@ -1,14 +1,20 @@
 package br.com.felipeDuarte.domain.dto;
 
 import java.io.Serializable;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class AluguelDTO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
 	private Integer idAluguel;
-	private Integer idLocacao;
 	
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private Date data;
+	
+	private Integer idLocacao;
 	
 	public AluguelDTO() {
 		
@@ -20,6 +26,14 @@ public class AluguelDTO implements Serializable{
 
 	public void setIdAluguel(Integer idAluguel) {
 		this.idAluguel = idAluguel;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
 	}
 
 	public Integer getIdLocacao() {
